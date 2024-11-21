8ea274d43c1d59727f3efc86db712572d0cd5f34
alex <alexsoriano00@gmail.com>
2024-11-21 15:14:47 +0100

    Timer and game over

--- index.js
+++ index.js
@@ -150,6 +150,44 @@ function updateAttackBoxDirection() {
     }
 }
 
+
+function determineWinner({player, enemy, timerId}){
+
+    clearTimeout(timerId)
+
+    document.querySelector('#displayText').style.display = 'flex'
+    
+    if (player.health === enemy.health ){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }else if(player.health > enemy.health ){
+        document.querySelector('#displayText').innerHTML = 'Player 1 WIN'
+    }else if(player.health < enemy.health ){
+        document.querySelector('#displayText').innerHTML = 'Player 2 WIN'
+    }
+}
+
+
+let timer = 60
+let timerId
+
+function decreaseTimer(){
+
+    if(timer > 0){
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer --  
+        document.querySelector('#timer').innerHTML = timer 
+    }
+
+    if(timer === 0){
+
+        determineWinner({player, enemy, timerId})
+    }
+    
+
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
 
@@ -201,6 +239,13 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 10
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+
+    //end game based on health
+
+    if(enemy.health <= 0 || player.health <= 0){
+        determineWinner({player, enemy, timerId})
     }
 }
 
